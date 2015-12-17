732f0f7a08c54c076105b76e33380c283379996c
continue <sozontoff.danil@yandex.ru>
2015-12-17 19:14:50 +0300

    fix charset

--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
-// ConsoleApplication9.cpp: определяет точку входа для консольного приложения.
+// ConsoleApplication9.cpp: РѕРїСЂРµРґРµР»СЏРµС‚ С‚РѕС‡РєСѓ РІС…РѕРґР° РґР»СЏ РєРѕРЅСЃРѕР»СЊРЅРѕРіРѕ РїСЂРёР»РѕР¶РµРЅРёСЏ.
 //
-
 #include "stdafx.h"
 #include <iostream>
 #include <math.h>
